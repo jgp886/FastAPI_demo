4fc70607c3aa4b5cde66267105a1a2ead114b765
Joseph Patton <joseph.g.patton@sherwin.com>
2023-01-27 13:49:33 -0500

    exercise9 files added

--- carsharing.py
+++ carsharing.py
@@ -1,40 +1,52 @@
-from typing import List, Optional
-
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import SQLModel
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-app = FastAPI()
-db = [
-    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
-    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
-    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
-    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
-    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+from db import engine
+from routers import cars, web, auth
+from routers.cars import BadTripException
+
+app = FastAPI(title="Car Sharing")
+app.include_router(web.router)
+app.include_router(cars.router)
+app.include_router(auth.router)
+
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8080",
 ]
 
-
-@app.get("/api/cars")
-def get_cars(size: str|None = None, doors: int|None = None) -> list:
-# def get_cars(size: Optional[str] = None, doors: Optional[str] = None) -> List:
-    result = db
-    if size:
-        result = [car for car in result if car['size'] == size]
-    if doors:
-        result = [car for car in result if car['doors'] >= doors]
-    return result
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/api/cars/{id}")
-def car_by_id(id: int) -> dict:
-    result = [car for car in db if car['id'] == id]
-    if result:
-        return result[0]
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.exception_handler(BadTripException)
+async def unicorn_exception_handler(request: Request, exc: BadTripException):
+    return JSONResponse(
+        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"message": "Bad Trip"},
+    )
+
+
+# @app.middleware("http")
+# async def add_cars_cookie(request: Request, call_next):
+#     response = await call_next(request)
+#     response.set_cookie(key="cars_cookie", value="you_visited_the_carsharing_app")
+#     return response
 
 
 if __name__ == "__main__":
